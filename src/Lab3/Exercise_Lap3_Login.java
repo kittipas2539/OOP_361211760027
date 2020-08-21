06c4f144bf3c28a4b72875b53736a1d7d1430bf0
bank <banksr2539@rmutsvmail.com>
2020-08-21 16:21:05 +0700

    Lab 3 Exercise3

---
+++ src/Lab3/Exercise_Lap3_Login.java
@@ -0,0 +1,38 @@
+package Lab3;
+
+import java.util.Scanner;
+
+public class Exercise_Lap3_Login {
+
+    public static void main(String[] args) {
+
+        Scanner sc  = new Scanner(System.in);
+
+        int num = 1234;
+        do {System.out.print("Welcome to MT Website ");
+        num = sc.nextInt();
+
+
+        }while (num != 456);
+            System.out.println("You account has been locked please, contect admin.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
